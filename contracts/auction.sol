287dbbf0651937599e08ee5272887cb80e20d595
Lucas Menendez <epucas@gmail.com>
2020-07-21 09:59:49 +0200

    some fixes

--- contracts/auction.sol
+++ contracts/auction.sol
@@ -15,18 +15,15 @@ contract SimpleAuction {
     event HighestBidIncreased(address bidder, uint amount);
     event AuctionEnded(address winner, uint amount);
 
-    public function SimpleAuction(
-        uint _biddingTime,
-        address _beneficiary
-    ) {
+    constructor (uint _biddingTime, address _beneficiary) public {
         beneficiary = _beneficiary;
         auctionStart = now;
         biddingTime = _biddingTime;
     }
 
-    public function bid() payable {
-        if (now > auctionStart + biddingTime) throw;
-        if (msg.value <= highestBid) throw;
+    function bid() public payable {
+        require (now < auctionStart + biddingTime, 'The auction has ended.');
+        require (msg.value > highestBid, 'The bid provided is lower than the highest one.');
 
         if (highestBidder != 0) {
             pendingReturns[highestBidder] += highestBid;
@@ -34,29 +31,31 @@ contract SimpleAuction {
 
         highestBidder = msg.sender;
         highestBid = msg.value;
-        HighestBidIncreased(msg.sender, msg.value);
+        emit HighestBidIncreased(msg.sender, msg.value);
     }
 
-    public function withdraw() returns (bool) {
-        var amount = pendingReturns[msg.sender];
+    function withdraw() public returns (bool){
+        uint amount = pendingReturns[msg.sender];
+
         if (amount > 0) {
             pendingReturns[msg.sender] = 0;
 
-            if (!msg.sender.send(amount)) {
+            if (!msg.sender.transfer(amount)) {
                 pendingReturns[msg.sender] = amount;
                 return false;
             }
         }
+
         return true;
     }
 
-    public function auctionEnd() {
-        if (now <= auctionStart + biddingTime) throw;
-        if (ended) throw;
+    function auctionEnd() public {
+        require (now > auctionStart + biddingTime, 'The auction is already expired.');
+        require (!ended, 'The auction is already ended.');
 
         ended = true;
-        AuctionEnded(highestBidder, highestBid);
+        emit AuctionEnded(highestBidder, highestBid);
 
-        if (!beneficiary.send(highestBid)) throw;
+        require(beneficiary.transfer(highestBid), 'Error transfering founds to the beneficiary');
     }
 }
