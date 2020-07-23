3e201ab17e41eb6f03d00bc062a66a8ad97fb9b6
Lucas Menendez <epucas@gmail.com>
2020-07-23 12:27:41 +0200

    last version with cancel function

--- contracts/auction.sol
+++ contracts/auction.sol
@@ -2,19 +2,21 @@ pragma solidity ^0.5.0;
 
 contract Auction {
     address payable public beneficiary;
-    address payable public admin;
+    address public admin;
     uint public auctionStart;
     uint public biddingTime;
 
-    address public highestBidder;
+    address payable public highestBidder;
     uint public highestBid;
 
     mapping(address => uint) pendingReturns;
+    address payable[] pendingAccounts;
 
     bool ended;
 
     event HighestBidIncreased(address bidder, uint amount);
     event AuctionEnded(address winner, uint amount);
+    event AuctionCanceled();
 
     constructor (
         uint _biddingTime, 
@@ -30,9 +32,12 @@ contract Auction {
     function bid() public payable {
         require (now < auctionStart + biddingTime, "The auction has ended.");
         require (msg.value > highestBid, "The bid provided is lower than the highest one.");
+        require (msg.sender != highestBidder, "You are already the highest bidder.");
+        require (msg.sender != admin, "The auction admin can't place a bid." );
 
         if (highestBidder != address(0)) {
             pendingReturns[highestBidder] += highestBid;
+            pendingAccounts.push(highestBidder);
         }
 
         highestBidder = msg.sender;
@@ -46,7 +51,9 @@ contract Auction {
         if (amount > 0) {
             pendingReturns[msg.sender] = 0;
 
-            if (!msg.sender.send(amount)) {
+            //if (!msg.sender.send(amount)) {
+            (bool success, ) = msg.sender.call.value(amount)("");
+            if (!success) {
                 pendingReturns[msg.sender] = amount;
                 return false;
             }
@@ -57,11 +64,34 @@ contract Auction {
 
     function auctionEnd() public {
         require (now < auctionStart + biddingTime, "The auction is already finished.");
+        require (msg.sender == admin, "You don't have permission to cancel the auction.");
         require (!ended, "The auction is already finished");
 
         ended = true;
         emit AuctionEnded(highestBidder, highestBid);
 
-        require (beneficiary.send(highestBid), "Error transfering founds to the beneficiary");
+        // require (beneficiary.send(highestBid), "Error transfering founds to the beneficiary");
+        (bool success, ) = beneficiary.call.value(highestBid)("");
+        require (success, "Error transfering founds to the beneficiary");
+    }
+
+    function auctionCancel() public {
+        require (msg.sender == admin, "You don't have permission to cancel the auction.");
+
+        ended = true;
+        emit AuctionCanceled();
+
+        for (uint i = 0; i < pendingAccounts.length; i++) {
+            address payable bidder = pendingAccounts[i];
+            uint amount = pendingReturns[bidder];
+
+            // require (bidder.send(amount), "Error placing pending returns");
+            (bool success, ) = bidder.call.value(amount)("");
+            require (success, "Error placing pending returns");
+        }
+
+        // require (highestBidder.send(highestBid), "Error placing pending returns");
+        (bool success, ) = highestBidder.call.value(highestBid)("");
+        require (success, "Error placing pending returns");
     }
 }
